3c27fdf2d3eaf615d64830423f52481c967b2d9f
zadzora <65119204+zadzora@users.noreply.github.com>
2021-02-24 21:10:36 +0100

    Add files via upload

---
+++ GUI2.py
@@ -0,0 +1,81 @@
+import os
+from tkinter import *
+from tkinter import filedialog
+import subtitlesTimer as subT
+
+
+global filename
+filename = "default.srt"
+
+timeMove = 0
+
+
+def timeValue(self):
+    global timeMove
+    timeMove = int(s1.get())
+
+
+def changeValues():
+    global filename
+    subT.Wooble(timeMove, filename)
+
+def browseFiles():
+    global filename
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select a File",
+                                          filetypes=(("Srt files", "*.srt*"), ("Text files", "*.txt*"), ("all files", "*.*")))
+
+    label_file_explorer.configure(text="File Opened: " + filename)
+    filename = os.path.basename(filename)
+
+
+if __name__ == '__main__':
+
+    # create window
+    window = Tk()
+    window.geometry(str(500) + 'x' + str(300))
+    window.title('Subtitle')
+    window.resizable(False, False)
+
+    title_label = Label(window, text='Subtitles Changer', font=('Verdana', 12, 'bold'))
+
+
+    # Create a File Explorer label
+    label_file_explorer = Label(window, text="Choose source file...", fg="blue")
+
+#
+    button_explore = Button(window,
+                            text="Browse Files",
+                            command=browseFiles)
+
+
+    timeToMove_label = Label(window, text="Time", font=('Verdana', 10))
+    timeToMove_field = Entry(window)
+
+
+
+    change_button = Button(window, text='Change', font=('Verdana', 10, 'bold'),
+                           bg="green",
+                           fg="white",
+                           command=changeValues)
+
+    v1 = DoubleVar()
+
+    s1 = Scale(window, variable=v1,
+               from_=-50, to=50,
+               orient=HORIZONTAL,
+               command=timeValue)
+
+
+    title_label.grid(column=2, row=1)
+    label_file_explorer.grid(column=1, row=2)
+    button_explore.grid(column=3, row=2)
+
+    timeToMove_label.grid(column=0, row=3)
+
+    s1.grid(column=1, row=3)
+    change_button.grid(column=2, row=5)
+
+
+    window.mainloop()
+
---
+++ subtitlesTimer.py
@@ -0,0 +1,91 @@
+import re
+import datetime
+import os
+
+
+def Convert(string):
+    li = list(string.split(" "))
+    return li
+
+def Wooble(time, fileName):
+
+    old_file_name = fileName
+
+    file = open(old_file_name , "r", encoding='iso-8859-1')
+    lines = file.readlines()
+    file.close()
+
+    text = ''
+    regex = '^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}'
+    oldTime = []
+    newTime = []
+    newFirst = []
+
+    timeWooble = time
+
+    for line in lines:
+
+        if re.search(regex, line, re.DOTALL) is not None:
+            text += line.rstrip('\n')
+            text += '\n'
+
+            both = re.findall('^[0-9]{2}:[0-9]{2}:[0-9]{2}.*', line)
+
+            first = re.search('[0-9]{2}:[0-9]{2}:[0-9]{2}(\,)[0-9]{3}', line)
+            second = re.search('[0-9]{2}:[0-9]{2}:[0-9]{2}(\,)[0-9]{3}$', line)
+
+            first = first.group()
+            first = str(first)
+
+            second = second.group()
+            second = str(second)
+
+            date_time_first = datetime.datetime.strptime(first, '%H:%M:%S,%f')
+            date_time_secnd = datetime.datetime.strptime(second, '%H:%M:%S,%f')
+
+            finalFirstTime = date_time_first + datetime.timedelta(0, timeWooble)
+
+            finalFirstTime = str(finalFirstTime)
+            finalFirstTime = finalFirstTime[12:-3]
+            finalFirstTime = f"{'0'}{finalFirstTime}"
+
+
+            finalSecndTime = date_time_secnd + datetime.timedelta(0, timeWooble)
+
+            finalSecndTime = str(finalSecndTime)
+            finalSecndTime = finalSecndTime[12:-3]
+            finalSecndTime = f"{'0'}{finalSecndTime}"
+
+            newFirst.append(first)
+
+            both = str(both)
+
+            oldTime.append(both)
+
+            both = both.replace(first, finalFirstTime)
+            both = both.replace(second, finalSecndTime)
+
+            # print(both)
+            newTime.append(both)
+
+
+    i = -1
+    final_file = ''
+
+    for line in lines:
+        if re.search('^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}', line) is not None:
+            i += 1
+
+        oldRep = str(oldTime[i])[2:-2]
+        newRep = str(newTime[i])[2:-2]
+
+        final_file += line.replace(oldRep, newRep)
+
+    username = os.getlogin()  # Fetch username
+    new_file_name = old_file_name + "_timed" + '.srt'
+
+    with open(f'C:\\Users\\{username}\\Desktop\\' + new_file_name, 'w', encoding='iso-8859-1') as f:
+        f.write(final_file)
+
+if __name__ == '__main__':
+    Wooble(4, "sub")
